a50d8ac060bfaf300f8bca6a67f9805fea0213d2
Ryan L. Bell <ryan.l.bell@gmail.com>
2017-09-30 16:36:59 -0400

    chore(releases): Stop checking in built js

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-var Task_1 = require("./Task");
-__export(require("./Task"));
-exports.default = Task_1.default;
-//# sourceMappingURL=index.js.map
